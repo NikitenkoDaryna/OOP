76846e89f68d2027de79526d6d7791f847db1b8b
NikitenkoDaryna <darynadobro@gmail.com>
2020-02-23 15:03:25 +0200

    adding OOP_Lab2

---
+++ MyLibrary/MyLibrary/_String.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public class _String
+    {
+        public void Welcome(string name)
+        {
+            Console.WriteLine("Hello "+name);
+        }
+    }
+}
